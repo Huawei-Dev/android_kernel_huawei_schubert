b3ae8fe74532f4dfc20f389f38e9937a35723520
Huawei-Dev <surdu_petru@yahoo.com>
2024-03-03 22:37:05 +0100

    arch:arm64: remove unused CONFIG_HUAWEI_KERNEL_STACK_NX

--- include/chipset_common/kernel_harden/kaslr.h
+++ include/chipset_common/kernel_harden/kaslr.h
@@ -17,19 +17,10 @@ extern void kti_randomize_init(void);
 extern void set_init_thread_info(unsigned long addr);
 #endif
 
-#ifndef CONFIG_HUAWEI_KERNEL_STACK_NX
-#define set_init_stack_nx(x)
-#define set_task_stack_nx(x)
-#else
 extern void __init set_init_stack_nx(struct thread_info *ti);
 extern void set_task_stack_nx(struct thread_info *ti);
-#endif
 
-#ifndef CONFIG_HUAWEI_KERNEL_STACK_NX
-#define set_init_stack_nx(x)
-#else
 extern void __init set_init_stack_nx(struct thread_info *ti);
-#endif
 
 extern unsigned long get_module_load_offset(void);
 
