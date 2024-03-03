345dcb2480fcb824c843967f720eebd12b7e4529
Huawei-Dev <surdu_petru@yahoo.com>
2024-03-03 22:38:55 +0100

    arch:arm64: remove unused CONFIG_HUAWEI_KERNEL_STACK_RANDOMIZE_STRONG

--- include/chipset_common/kernel_harden/kaslr.h
+++ include/chipset_common/kernel_harden/kaslr.h
@@ -8,14 +8,9 @@ extern unsigned int kstack_offset;
 extern void kstack_randomize_init(void);
 #endif
 
-#ifndef CONFIG_HUAWEI_KERNEL_STACK_RANDOMIZE_STRONG
-#define kti_randomize_init()
-#define set_init_thread_info(x)
-#else
 extern unsigned long kti_offset;
 extern void kti_randomize_init(void);
 extern void set_init_thread_info(unsigned long addr);
-#endif
 
 extern void __init set_init_stack_nx(struct thread_info *ti);
 extern void set_task_stack_nx(struct thread_info *ti);
