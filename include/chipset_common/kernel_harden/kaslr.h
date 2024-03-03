156a7fa9255fe1073d8cbb676b26549f7f123a5d
Huawei-Dev <surdu_petru@yahoo.com>
2024-03-03 22:34:43 +0100

    arch:arm64: remove unused CONFIG_HUAWEI_KERNEL_MODULE_RANDOMIZE

--- include/chipset_common/kernel_harden/kaslr.h
+++ include/chipset_common/kernel_harden/kaslr.h
@@ -31,10 +31,6 @@ extern void set_task_stack_nx(struct thread_info *ti);
 extern void __init set_init_stack_nx(struct thread_info *ti);
 #endif
 
-#ifndef CONFIG_HUAWEI_KERNEL_MODULE_RANDOMIZE
-#define get_module_load_offset()
-#else
 extern unsigned long get_module_load_offset(void);
-#endif
 
 #endif
