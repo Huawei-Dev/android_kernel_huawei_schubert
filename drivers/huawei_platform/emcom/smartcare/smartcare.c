28b18c8ea26a593ec2db7759d32c9e8040bfb440
Huawei-Dev <surdu_petru@yahoo.com>
2024-03-03 15:06:16 +0100

    arch:arm64: remove unused CONFIG_HW_SMARTCARE_FI

--- drivers/huawei_platform/emcom/smartcare/smartcare.c
+++ drivers/huawei_platform/emcom/smartcare/smartcare.c
@@ -9,22 +9,10 @@
 #include "../emcom_utils.h"
 #include <huawei_platform/emcom/smartcare/smartcare.h>
 
-#ifdef CONFIG_HW_SMARTCARE_FI
-#include <huawei_platform/emcom/smartcare/fi/fi.h>
-#endif
-
-
 void smartcare_event_process(int32_t event, uint8_t *pdata, uint16_t len)
 {
 	switch (event)
 	{
-		#ifdef CONFIG_HW_SMARTCARE_FI
-		case NETLINK_EMCOM_DK_SMARTCARE_FI_APP_LAUNCH:
-		case NETLINK_EMCOM_DK_SMARTCARE_FI_APP_STATUS:
-			fi_event_process(event, pdata, len);
-			break;
-		#endif
-
 		default:
 			EMCOM_LOGE(" : smartcare received unsupported message");
 			break;
@@ -34,20 +22,12 @@ void smartcare_event_process(int32_t event, uint8_t *pdata, uint16_t len)
 
 void smartcare_init(void)
 {
-	#ifdef CONFIG_HW_SMARTCARE_FI
-	fi_init();
-	#endif
-
 	return;
 }
 
 
 void smartcare_deinit(void)
 {
-	#ifdef CONFIG_HW_SMARTCARE_FI
-	fi_deinit();
-	#endif
-
 	return;
 }
 
