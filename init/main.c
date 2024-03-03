43cf59b871954b6c504cb049660acdba8d4db1c5
Huawei-Dev <surdu_petru@yahoo.com>
2024-03-03 14:45:11 +0100

    arch:arm64: remove unused CONFIG_HUAWEI_BOOT_TIME

--- init/main.c
+++ init/main.c
@@ -92,9 +92,6 @@
 #ifdef CONFIG_HW_RECLAIM_ACCT
 #include <chipset_common/reclaim_acct/reclaim_acct.h>
 #endif
-#ifdef CONFIG_HUAWEI_BOOT_TIME
-#include <huawei_platform/boottime/hw_boottime.h>
-#endif
 static int kernel_init(void *);
 
 extern void init_IRQ(void);
@@ -821,11 +818,7 @@ int __init_or_module do_one_initcall(initcall_t fn)
 	if (initcall_debug)
 		ret = do_one_initcall_debug(fn);
 	else
-#ifdef CONFIG_HUAWEI_BOOT_TIME
-		ret = do_boottime_initcall(fn);
-#else
 		ret = fn();
-#endif
 
 	msgbuf[0] = 0;
 
@@ -1006,9 +999,6 @@ static int __ref kernel_init(void *unused)
 	rcu_end_inkernel_boot();
 
 	pr_err("Kernel init end, jump to execute /init\n");
-#ifdef CONFIG_HUAWEI_BOOT_TIME
-	boot_record("[INFOR] Kernel_init_done");
-#endif
 	if (ramdisk_execute_command) {
 		ret = run_init_process(ramdisk_execute_command);
 		if (!ret)
